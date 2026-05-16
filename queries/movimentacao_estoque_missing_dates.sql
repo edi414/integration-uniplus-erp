efbce33ef296e2c6ff4acd116eac9a4cc06f27bf
edi414 <edivaldo414@gmail.com>
2026-05-16 15:19:05 -0300

    fix: update missing dates query to include records from the last seven days

--- queries/movimentacao_estoque_missing_dates.sql
+++ queries/movimentacao_estoque_missing_dates.sql
@@ -2,5 +2,6 @@ SELECT DISTINCT cs."date"
 FROM company_schedule cs
 LEFT JOIN movimentacao_estoque me ON DATE(me.datahora) = cs."date"
 WHERE me.datahora IS NULL
+   OR cs."date" >= CURRENT_DATE - INTERVAL '7 days'
 ORDER BY cs."date";
 
